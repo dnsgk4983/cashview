7fd6babf936e9601003bcc3b06499c7dbf5afc3c
dnsgk4983 <64947285+dnsgk4983@users.noreply.github.com>
2020-11-06 00:20:21 +0900

    cash

--- js/script.js
+++ js/script.js
@@ -1,17 +1,90 @@
-$(function(){
-    let cash = '.left-area li'
-    $(cash).on('click', function(){
-        $(cash).removeClass('active');
-        $(this).addClass('active');
+$(document).ready(function () {
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+    
+    var prx = document.getElementById('prChart').getContext('2d');
+
+    var pchart = new Chart(prx, {
+        type: 'line',
+
+        data: {
+            labels: ['', '10.20', '10.20', '10.20', '10.20', '10.20', '10.20', '10.20', ""],
+            datasets: [{
+                label: "",
+                backgroundColor: 'transparent',
+                borderColor: '#FF8200',
+                fill: true,
+                data: [200, 200, 300, 100, 400, 500, 400, 300, 300],
+            }
+        ]},
+        options: {
+            legend: {
+                display: false
+            },
+            scales: {
+                xAxes: [{
+                    gridLines: {
+                        color: 'rgba(0,0,0,0)',
+                        lineWidth: 0
+                    }
+                }],
+                yAxes: [{
+                    display: true,
+                    ticks: {
+                        min: 0,
+                        stepSize: 100,
+                        fontSize: 13,
+                        fontColor: '#878787'
+                    },
+                }]
+            }
+        }
     });
 
-    $('button.navbar-toggle').on('click',function(){
-        $('ul.navbar-nav').slideToggle();
-    });
+    var arx = document.getElementById('adChart').getContext('2d');
 
-    let mf = '.info-mf a';
-    $(mf).on('click', function(){
-        $(mf).removeClass('active');
-        $(this).addClass('active');
-    });
+    var achart = new Chart(arx, {
+        type: 'line',
+
+        data: {
+            labels: ['', '10.20', '10.20', '10.20', '10.20', '10.20', '10.20', '10.20', ""],
+            datasets: [{
+                label: "",
+                backgroundColor: "transparent",
+                borderColor: '#4C9EFF',
+                fill: false,
+                data: [300, 300, 330, 300, 380, 500, 420, 400, 390]
+            },
+            {
+                label: "",
+                backgroundColor: "transparent",
+                borderColor: "#FF8200",
+                fill: false,
+                data: [100, 100, 200, 0, 300, 400, 300, 240, 230]
+            }
+            ]},
+        options: {
+            legend: {
+                display: false,
+            },
+            scales: {
+                xAxes: [{
+                    gridLines: {
+                        color: 'rgba(0,0,0,0)',
+                        lineWidth: 0
+                    }
+                }],
+                yAxes: [{
+                    display: true,
+                    ticks: {
+                        min: 0,
+                        stepSize: 100,
+                        fontSize: 13,
+                        fontColor: '#878787'
+                    },
+                }]
+            }
+        }
+    })
 });
